a6169665ba31d28d8e26a69274c6c6c44a829ce6
stdrosebot <104678019+stdrosebot@users.noreply.github.com>
2022-08-23 20:52:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ bot = Client(
 
 @bot.on_message(filters.command(["start"], prefixes=["/", "!"]))
 async def start(client, message):
-        await message.reply_text("Hi! My name is kanishka")
+        await message.reply_text("Hi! My name is Shikha")
 
 
 @bot.on_message(
